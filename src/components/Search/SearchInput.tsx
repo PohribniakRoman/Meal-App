44187e990ce13f0030eb8e3c48230e69ea97c537
PohribniakRoman <79878288+PohribniakRoman@users.noreply.github.com>
2023-07-02 18:34:59 +0300

    add fix to dishpage

--- src/components/Search/SearchInput.tsx
+++ src/components/Search/SearchInput.tsx
@@ -10,7 +10,8 @@ export const SearchInput:React.FC<InputCotroller> = ({value,setValue})=>{
     return <div className="search-modal__input--container">
     <TextField autoFocus variant="outlined" name="meal-search" label="Dish name" className="search-modal__input" value={value} onChange={(event)=>{
         const element = event.target as HTMLInputElement;
-        setValue(element.value.replace(/[.*+?^${}()|[\]\\1-9-+/0=]/g,""));
+        const string = element.value.replace(/[.*+?^${}()|[\]\\1-9-+/0:,;'"`~=]/g,"");
+        setValue(string?string:"");
     }}/>
     <Typography className={`search-modal__input--clear ${value?"shown":"hidden"}`} onClick={()=>setValue("")}><AiFillDelete/></Typography>
 </div>
