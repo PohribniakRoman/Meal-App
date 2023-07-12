74c56ce4cf88287a6a35abf5017d8d33f02c942c
PohribniakRoman <79878288+PohribniakRoman@users.noreply.github.com>
2023-07-12 23:25:30 +0300

    last fix

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -12,12 +12,13 @@ export const Cart:React.FC = () =>{
     return <section className="page">
         <Navbar/>
         <div className="page__container fullsize">
-        <Typography className="page__title">Cart / {cart.price}<BiDollar/></Typography>
-        <div className="side-cart__container">
+        <Typography className="page__title">Cart </Typography>
+            <div className="side-cart__container">
                 {!cart.products.length ?<Typography variant="h2" className="cart__placeholder">There is nothing in your cart:(</Typography> :cart.products.map((product)=>{
                     return <Dish key={product.idMeal} product={product}/>
                 })}
             </div>
+            <Typography className="side-cart__price" sx={{textAlign:"center"}}> {cart.price}<BiDollar/></Typography>
         </div>
     </section>
 }
